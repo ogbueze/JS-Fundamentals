3f48ca05af1fa46930e0c053d4a95de184365757
ogbueze <ogbuezeokenyi@gmail.com>
2025-11-09 11:13:45 +0100

    add argument file to check the number of arguments

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,7 +1,7 @@
-const argument = prompt("pass and argument");
-if (argument.length > 1) {
+const numArgument = process.argv.length - 2;
+if (numArgument > 1) {
 	console.log("Arguments found");
-} else if (argument.length == 0) {
+} else if (numArgument == 0) {
 	console.log("No argument");
 } else {
 	console.log("Argument found");
