b36feef26fc6a41a32cc62f30d289d58351c843c
ogbueze <ogbuezeokenyi@gmail.com>
2025-11-09 11:17:22 +0100

    I commit

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,8 +1,8 @@
 const numArgument = process.argv.length - 2;
-if (numArgument > 1) {
-	console.log("Arguments found");
+if (numArgument == 1) {
+	console.log("Argument found");
 } else if (numArgument == 0) {
 	console.log("No argument");
 } else {
-	console.log("Argument found");
+	console.log("Arguments found");
 }
