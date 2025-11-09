b9e6af5f73f47fdc1c3ae43894b12a18e2da8c52
ogbueze <ogbuezeokenyi@gmail.com>
2025-11-09 16:01:23 +0100

    debug

--- 10-factorial.js
+++ 10-factorial.js
@@ -2,7 +2,7 @@ const num = process.argv[2];
 
 function factorial(n) {
 	if (isNaN(Number(n)) || !Number.isInteger(Number(n)) || Number(n) < 0) {
-		console.log("Factorial is not defined for negative numbers");
+		console.log(1);
 	} else {
 		let result = 1;
 		for (let i = 1; i <= n; i++) {
